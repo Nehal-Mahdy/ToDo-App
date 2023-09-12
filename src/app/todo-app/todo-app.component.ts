d90fbc00c82da0c9afa0345cee5bad3f9f39a80d
Nehal-Mahdy <nehalhassan914@yahoo.com>
2023-09-13 02:20:04 +0300

    added all second time

--- src/app/todo-app/todo-app.component.ts
+++ src/app/todo-app/todo-app.component.ts
@@ -9,21 +9,14 @@ import { Todo } from '../Todo';
 export class TodoAppComponent {
 todos: Todo[]=[];
 
+//recieved from form component
 recievedFromChild(newTodo : string){
   let todo = new Todo();
   todo.name= newTodo;
   todo.isCompleted=true;
   this.todos.push(todo);
 }
-
-// done(id: number){
-//   this.todos[id].isCompleted= !this.todos[id].isCompleted;
-// }
-
-// remove(id: number){
-//   this.todos = this.todos.filter((v, i) => i !== id);
-// }
-
+//removed by todolist component 
 removedFromchild(index : number){
   this.todos = this.todos.filter((v, i) => i !== index);
 }
